286cadad37dd3da8c6a4946653ee7b77516b3483
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-10-23 15:35:53 +0530

    fragment inside fragment

--- app/src/main/java/com/learningandroid/protradin/Trips.java
+++ app/src/main/java/com/learningandroid/protradin/Trips.java
@@ -2,11 +2,15 @@ package com.learningandroid.protradin;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +23,9 @@ public class Trips extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    View myFragment;
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +66,54 @@ public class Trips extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trips, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_trips, container, false);
+
+        viewPager = myFragment.findViewById(R.id.viewpager);
+        tabLayout = myFragment.findViewById(R.id.tablayout);
+
+        return myFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        tripsAdapter adapter = new tripsAdapter(getChildFragmentManager());
+        adapter.addFragment(new Active(),"ACTIVE");
+        adapter.addFragment(new Booked(),"BOOKED");
+        adapter.addFragment(new History(),"HISTORY");
+
+        viewPager.setAdapter(adapter);
     }
 }
+
+
+
+
+
+
+
+
+
+
